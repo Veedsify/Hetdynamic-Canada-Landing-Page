49d1dbf2f4a500ddebbe3cf97f207a5bfc59dfc5
= <=>
2025-07-05 20:36:41 +0100

    Fix import paths for ContactUs component in HomeFour and HomeSix

--- src/page/home/HomeSix.jsx
+++ src/page/home/HomeSix.jsx
@@ -1,5 +1,5 @@
 import About from "../../components/home/home-six/about";
-import ContactUs from "../../components/home/home-sixhttps://calendly.com/hetdynamic/get-the-canada-residency-permit-for-entrepreneurs";
+import ContactUs from "../../components/home/home-six/contact-us";
 import Counter from "../../components/home/home-six/counter";
 import Faq from "../../components/home/home-six/faq";
 import Features from "../../components/home/home-six/features";
